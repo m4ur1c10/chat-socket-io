b559d0a33c161edb53963a92c51839214af73780
Mauricio Machado <mauricio@tibox.com.br>
2019-03-13 11:11:19 -0300

    commit initial

---
+++ index.js
@@ -0,0 +1,44 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+var users = [];
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/nickname.html');
+});
+
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/users', function(req, res){
+  res.set("Content-type", "application/json");  
+  res.send(JSON.stringify(users));
+});
+
+io.on('connection', function(socket){
+  socket.on('user logged', function(user){
+    users.push(user);
+    io.emit('new user', user);
+  });
+  socket.on('user logout', function(user){
+    users = users.filter(function(user_){
+      if (user_ != user) {
+        return user_;
+      }
+    });
+    io.emit('user logout', user);
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
