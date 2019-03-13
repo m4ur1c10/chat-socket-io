a25a1b190a739637b7b99d810d77a4b5a36d4fd1
Mauricio Machado <mauricio@tibox.com.br>
2019-03-13 12:00:21 -0300

    folder public correcao

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 var users = [];
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/nickname.html');
